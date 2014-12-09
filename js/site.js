c1a39fc424cf1c4717810106f6bbb79eca91076d
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-12-08 17:08:20 -0800

    Concatenate javscripts

---
+++ js/site.js
@@ -0,0 +1,9 @@
+---
+---
+{% include_relative foundation/foundation.js %}
+{% include_relative foundation/foundation.orbit.js %}
+{% include_relative foundation/foundation.topbar.js %}
+{% include_relative foundation/foundation.dropdown-custom.js %}
+{% include_relative vendor/custom.modernizr.js %}
+{% include_relative lib/responsive.js %}
+{% include_relative lib/jquery.fix-aspect-ratio.js %}
