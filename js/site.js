715671e854c46f66821c0eb7786446a6226bc094
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-12-08 21:40:05 -0800

    Ge modernizr before other scripts

--- js/site.js
+++ js/site.js
@@ -1,8 +1,8 @@
 ---
 ---
+{% include_relative vendor/custom.modernizr.js %}
 {% include_relative foundation/foundation.js %}
 {% include_relative foundation/foundation.orbit.js %}
 {% include_relative foundation/foundation.topbar.js %}
 {% include_relative foundation/foundation.dropdown-custom.js %}
-{% include_relative vendor/custom.modernizr.js %}
 {% include_relative lib/responsive.js %}
