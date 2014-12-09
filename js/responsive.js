17412ecd0bf3ecf058c292c71b076c1e9961ec39
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-12-08 17:01:08 -0800

    Clean up responsive image code

--- js/responsive.js
+++ js/responsive.js
@@ -1,49 +1,52 @@
-;(function(root) {
+;
+(function($, window, document, undefined) {
 
-    var loadResponsiveImages = root.loadResponsiveImages = function(options) {
-        options = options || {};
+    var scriptName = 'loadResponsiveImages'
+
+    window[scriptName] = function() {
+        var options = arguments[1],
+            elements;
+
+        if ($ && this instanceof $) elements = this;
+
+        if (arguments[0] instanceof HTMLCollection || arguments[0] instanceof NodeList) {
+            elements = arguments[0];
+        } else if (arguments[0] instanceof Element) {
+            elements = [arguments[0]];
+        } else {
+            elements = document.getElementsByTagName('IMG');
+            options = options || arguments[0] || {};
+        }
 
         var sizes = options.sizes || [200, 300, 400, 600, 800, 1000, 1200, 1600],
-            retina = window.devicePixelRatio > 1 ? '@2x' : '',
-            width;
+            hostname = options.hostname || 'http://images.digital-democracy.org',
+            retina = window.devicePixelRatio > 1 ? '@2x' : '';
 
         var images = document.getElementsByTagName('IMG');
 
         for (var i = 0; i < images.length; i++) {
-            width = images[i].clientWidth;
-            if (width > 0) {
-                loadResponsive.call(images[i]);
-            } else {
-                images[i].addEventListener('load', loadResponsive);
-            }
+            onHasWidth(images[i], loadResponsive);
         }
 
         function loadResponsive(e) {
             var image = this,
-                width = image.clientWidth,
                 responsiveImage = new Image(),
                 responsiveWidth = sizes[0],
                 i = 1,
-                hostname,
                 url;
 
-            if (e) console.log("loaded", this.src, width);
-
-            image.removeEventListener('load', loadResponsive);
-
-            while (i < sizes.length && width > sizes[i - 1]) {
+            while (i < sizes.length && image.clientWidth > sizes[i - 1]) {
                 responsiveWidth = sizes[i];
                 i++;
             }
 
-            hostname = 'http://images.digital-democracy.org';
             url = image.src.replace(/^https?:\/\/[^\/]*/, hostname);
             url = url.split('?').shift();
-            url = url.replace(/(\.[^\.]{1,4})$/,'-' + responsiveWidth + retina + '$1');
+            url = url.replace(/(\.[^\.]{1,4})$/, '-' + responsiveWidth + retina + '$1');
 
             responsiveImage.onload = function() {
                 this.onload = null;
-                console.log('setting source from %s to %s', image.src, url);
+                // console.log('setting source from %s to %s', image.src, url);
                 image.src = url;
             };
 
@@ -56,4 +59,32 @@
         }
     };
 
-})(this);
+    if ($) $.fn[scriptName] = window[scriptName];
+
+    function onHasWidth(img, callback) {
+        if (img.clientWidth > 0) {
+            hasWidth.call(img);
+        } else {
+            var intervalID = window.setInterval(monitor, 10);
+            img.addEventListener('load', hasWidth);
+            img.addEventListener('error', stopMonitoring);
+        }
+
+        function stopMonitoring() {
+            window.clearInterval(intervalID);
+            img.removeEventListener('load', hasWidth);
+            img.removeEventListener('error', stopMonitoring);
+        }
+
+        function monitor() {
+            if (!img.clientWidth) return;
+            hasWidth.call(img);
+        }
+
+        function hasWidth() {
+            stopMonitoring();
+            callback.call(img);
+        }
+    }
+
+})($, window, document);
