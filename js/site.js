f88a9ac0bacb86f95d3dfa37cd87c6381d673651
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-12-08 17:21:46 -0800

    Switch to Zepto.js

--- js/site.js
+++ js/site.js
@@ -6,4 +6,5 @@
 {% include_relative foundation/foundation.dropdown-custom.js %}
 {% include_relative vendor/custom.modernizr.js %}
 {% include_relative lib/responsive.js %}
+{% include_relative lib/zepto.iodim.js %}
 {% include_relative lib/jquery.fix-aspect-ratio.js %}
