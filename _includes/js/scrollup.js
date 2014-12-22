e53f6ac11c990421ca0896064bb281f9f0634339
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-12-22 15:45:05 -0800

    remove console.log

--- _includes/js/scrollup.js
+++ _includes/js/scrollup.js
@@ -3,6 +3,5 @@
       offset = body.clientHeight / 3;
 
   if (body.clientWidth < 1024) return console.log('returning');
-  console.log('scrolling to ', offset);
   window.scrollTo(0, offset);
 })();
