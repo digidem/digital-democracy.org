b82c0eb89813b0787eaf169a691d51c60f618962
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-12-22 15:48:15 -0800

    Add video layout

---
+++ js/lib/video-setup.js
@@ -0,0 +1,29 @@
+(function() {
+
+    if (typeof $f === 'undefined') return;
+
+    var player = $f('video-iframe'),
+        $body = $('body');
+
+    player.addEvent('ready', function() {
+        player.addEvent('pause', onPause);
+        player.addEvent('play', onPlay);
+        player.addEvent('finish', onPause);
+        $('.play-button').on('click', function() {
+          player.api('play');
+        });
+    });
+
+    function onPause() {
+        $body.removeClass('video-playing');
+        $body.addClass('video-paused');
+        smoothScroll(document.body.clientHeight / 3);
+    }
+
+    function onPlay() {
+        smoothScroll(0, function() {
+            $body.addClass('video-playing');
+            $body.removeClass('video-paused');
+        });
+    }
+})();
