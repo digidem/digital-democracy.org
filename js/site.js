dc96ac9a698883e285dcc5cdfc0a8f7b4eceb402
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-12-23 16:32:26 -0800

    Attempt to fix strange page build failure with github

--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
 ---
 ---
-{% include_relative vendor/affix.js %}
 {% include_relative foundation/foundation.js %}
 {% include_relative foundation/foundation.orbit.js %}
 {% include_relative foundation/foundation.topbar.js %}
 {% include_relative foundation/foundation.dropdown-custom.js %}
+{% include_relative vendor/custom.affix.js %}
 {% include_relative lib/image-has-dim.js %}
 {% include_relative lib/responsive.js %}
 {% include_relative lib/smooth-scroll.js %}
